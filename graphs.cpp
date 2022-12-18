8331aeca9762b79a5c85d258e1a503c0fdfc9fc4
Adam Czyżak <101229929+VadamC27@users.noreply.github.com>
2022-12-18 01:41:55 +0100

    graphs

--- graphs.cpp
+++ graphs.cpp
@@ -1,6 +1,9 @@
 #include "graphs.h"
 #include <vector>
+#include <array>
 #include <iostream>
+#include <ctime>
+#include <stdlib.h>
 
 Graph::Graph(int s){
     size = s;
@@ -11,6 +14,8 @@ Graph::Graph(int s){
         }
         graph.push_back(row);
     }
+    longestCycleSize = 0;
+    return;
 }
 
 void Graph::showMatrix() {
@@ -20,4 +25,141 @@ void Graph::showMatrix() {
         }
         std::cout<<"\n";
     }
+    return;
 }
+
+void Graph::generateInput(){
+
+    int a;
+    std::vector<int> v;
+    for(int i =0; i<size;i++){
+        for(int j =0; j<size;j++){
+            do{
+                std::cout<<"Podaj wartosc (1 lub 0) dla pozycji ["<<i<<"]["<<j<<"]: "; 
+                std::cin>>a;
+            }while(!(a==0 || a==1));
+            v.push_back(a);
+        }
+        graph[i].assign(v.begin(),v.end());
+        v.clear();
+    }
+}
+
+void Graph::generateRing(){
+    int position;
+    for(int i = 0; i< size;i++){
+        position = (i+1)%size;
+        graph[i][position] = 1;
+    }
+    return;
+}
+
+void Graph::generateRing(int offset){
+    int position;
+    for(int i = 0; i< size;i++){
+        position = (i+offset)%size;
+        graph[i][position] = 1;
+    }
+    return;
+}
+
+void Graph::generateRandomEdges(float _saturation){
+        
+    int counter = 0, currentRowIndex, fill;
+    int saturation =  ((size * (size - 1)/2)* _saturation);
+    while(counter < saturation){
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j <size; j++){
+                fill = rand()%2;
+                if(fill && i!=j && !graph[i][j] ) {
+                    graph[i][j] = 1;
+                    counter++;
+                }
+            }
+        }
+    }
+    return;
+}
+
+void Graph::dfsLongestCycle(int v, int * visited,  std::vector<int>& current){
+    visited[v] =1;
+    current.push_back(v);    
+    for(int i = 0; i<size; i++){
+        if(graph[v][i] == 1  ){
+            if(i == current[0]&&current.size() > longestCycleSize){
+                current.push_back(i);
+                longestCycleSize = current.size();
+                longestCycle.clear();
+                longestCycle.assign(current.begin(), current.end());
+                break;
+            }
+            if( visited[i] == 0) dfsLongestCycle(i,visited,current);
+        }
+    }
+    current.pop_back();
+    return;
+}
+
+void Graph::findLongestCycle(){
+    int visited[size];
+    for(int i = 0; i<size; i++) visited[i] = 0;
+    std::vector<int> current;
+    
+    for(int i = 0; i<size; i++){
+        dfsLongestCycle(i, visited, current);
+        current.clear();
+    }
+
+    std::cout<<"The longest cycle: ";
+    for(int i = 0; i<longestCycle.size();i++){
+        std::cout<<longestCycle[i]<<" ";
+    }
+     std::cout<<"\n\n";
+    return;
+}
+
+bool Graph::dfsCheckCyclic(int v, int * visited,  std::vector<int>& current){
+    visited[v] =1;
+    current.push_back(v);
+    
+    for(int i = 0; i<size; i++){
+        
+        if(graph[v][i] == 1  ){
+            if(i == current[0]){
+                current.push_back(i);
+                std::cout<<"Cycle found!: ";
+                for (const auto& element : current)
+                {
+                    std::cout << element << " ";
+                }
+                return 1;
+            }
+           
+            if( visited[i] == 0) {
+                if(dfsCheckCyclic(i,visited,current))return true;
+            }
+
+        }
+    }
+    current.pop_back();
+    return false;
+}
+
+
+
+void Graph::findAnyCycle(){
+    int visited[size];
+    for(int i = 0; i<size; i++) visited[i] = 0;
+    std::vector<int> current;
+    
+    for(int i = 0; i<size; i++){
+        if(dfsCheckCyclic(i, visited, current)) return;
+        current.clear();
+    }
+    std::cout<<"\nGiven graph is acyclic.\n\n";
+    return;
+}
+
+
+
+int Graph::getSize(){return size;}
--- graphs.h
+++ graphs.h
@@ -5,11 +5,26 @@
 
 class Graph{
     int size;
+    int longestCycleSize;
+    std::vector<int> longestCycle;    
+    bool dfsCheckCyclic(int v, int * visited,  std::vector<int>& current);
+    void dfsLongestCycle(int v, int * visited,  std::vector<int>& current);
+
 public:
     std::vector<std::vector<int>> graph;
-    void showMatrix();
+    
+    //functions
+    void findLongestCycle();
+    void findAnyCycle();
+    void generateInput();       
+    void generateRing();
+    void generateRing(int offset);
+    void generateRandomEdges(float _saturation);      
+    void showMatrix();    
+    
+    int getSize();
+     
     Graph(int s);
 };
-
 
 #endif
