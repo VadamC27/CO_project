6d4ad0b2081e316eac2d4ed0d6a1da131e9ab81c
Adam Czyżak <101229929+VadamC27@users.noreply.github.com>
2022-12-18 16:58:06 +0100

    tests

--- main.cpp
+++ main.cpp
@@ -6,6 +6,20 @@
 #include <cstdio>
 
 #include "graphs.h"
+#include "testing.h"
+
+void getLength(Graph * graph){
+    int a;
+    std::cout<<"\nEnter length you want to look for: ";
+    do{
+        std::cin>>a;
+        if(a>2 && a<=graph->getSize()){
+            graph->findExactCycle(a);
+            return;
+        }
+        std::cout<<"Input correct value (at least 3 to the maxium being number of vertices: ";
+    }while(1);
+}
 
 void findCycle(Graph * graph){
     int a;
@@ -15,7 +29,7 @@ void findCycle(Graph * graph){
         switch(a){
             case 1:graph->findAnyCycle();break;
             case 2:graph->findLongestCycle();break;
-            case 3:
+            case 3:getLength(graph);break;
             default: std::cout<<"\nInput correct value:  ";  a=-1;
         }
      } while(a==-1);
@@ -31,6 +45,22 @@ void randoomInput(){
     std::cout<<"Enter saturation(0.0 -> 1.0): ";
     std::cin>>sat;
     (*graph).generateRandomEdges(sat);
+    std::cout<<"\nYour matrix:\n\n";
+    graph->showMatrix();
+    findCycle(graph);
+    delete graph;
+    return;
+}
+
+void acyclicInput(){
+        int s;
+    float sat;
+    std::cout<<"Enter number of vertices:";
+    std::cin>>s;
+    Graph * graph = new Graph(s);
+    std::cout<<"Enter saturation(0.0 -> 1.0): ";
+    std::cin>>sat;
+    (*graph).generateAcyclicGraph(sat);
     std::cout<<"\nYour matrix:\n\n";
     graph->showMatrix();
     findCycle(graph);
@@ -57,7 +87,7 @@ void ringInput(){
     std::cout<<"Enter number of vertices: ";
     std::cin>>s;
     Graph * graph = new Graph(s);
-    (*graph).generateRing(2);
+    (*graph).generateRing();
     graph->showMatrix();
     findCycle(graph);
     delete graph;
@@ -65,29 +95,61 @@ void ringInput(){
 }
 
 
-int main(){
-    // srand(time(NULL));
-    // std::cout<<"Hey!\n";
-    // Graph a(5);
-    // std::cout<<"Same 0:\n";
-    // a.showMatrix();
-    // a.generateRandomEdges(0.5);
-    // std::cout<<"Losowe 50% krawedzi\n";
-    // a.showMatrix();
-    // a.input();
-    // a.showMatrix();
+void mainUserMenu(){
+  
     int a;
-    
+   
         
     do{
-         std::cout<<"Chose an option\n  1. Random matrix \n  2. User input matrix\n  3. Ring matrix \n  4. End program\nOption: ";
+        std::cout<<"Chose an option\n  1. Random matrix \n  2. User input matrix\n  3. Ring matrix\n  4. Acyclic Graph \n  5. End program\nOption: ";
         std::cin>>a;
         switch(a){
             case 1: randoomInput();break;
             case 2: userInput();break;
             case 3: ringInput();break;
-            case 4: {std::cout<<"\nEnding work.. \n\n"; return 0;}
+            case 4: acyclicInput();break;
+            case 5: {std::cout<<"\nEnding work.. \n\n"; return;}
             default: std::cout<<"\nInput correct value:  ";
         }
     }while(1);    
+}
+
+void testing(){
+     int s = 50, e = 1000, g = 50;
+    // std::cout<<"Random graph 50 percent saturation: \n";
+    // GraphTest test1 = GraphTest(1,1,s,e,g);
+    // GraphTest test2 = GraphTest(2,1,s,e,g);
+    // GraphTest test3 = GraphTest(3,1,s,e,g);
+    // std::cout<<"Random graph 30 percent saturation: \n";
+    // GraphTest test4 = GraphTest(1,2,s,e,g);
+    // GraphTest test5 = GraphTest(2,2,s,e,g);
+    // GraphTest test6 = GraphTest(3,2,s,e,g);
+    std::cout<<"Random graph 80 percent saturation: \n";
+    GraphTest test7 = GraphTest(1,3,s,e,g);
+    GraphTest test8 = GraphTest(2,3,s,e,g);
+    GraphTest test0 = GraphTest(3,3,s,e,g);
+    // std::cout<<"Random acyclic graph 50 percent saturation: \n";
+    // GraphTest test9 = GraphTest(1,4,s,e,g);
+    // GraphTest test10 = GraphTest(2,4,s,e,g);
+    // GraphTest test11 = GraphTest(3,4,s,e,g);
+    // std::cout<<"Random graph with one cycle containing all of the vertices: \n";
+    // GraphTest test12 = GraphTest(1,5,s,e,g);
+    // GraphTest test13 = GraphTest(2,5,s,e,g);
+    // GraphTest test14 = GraphTest(3,5,s,e,g);
+}
+
+int main(){
+    srand(time(NULL));
+    int a; 
+        
+    do{
+        std::cout<<"Cycles in graph testing program.\n  1. User menu\n  2. Start testes\n  3. End program\nOption: ";
+        std::cin>>a;
+        switch(a){
+            case 1: mainUserMenu();break;
+            case 2: testing();break;
+            case 3: {std::cout<<"\nEnding work.. \n\n"; return 0;}
+            default: std::cout<<"\nInput correct value:  ";
+        }
+    }while(1); 
 }
